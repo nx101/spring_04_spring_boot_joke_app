e6eb4fd1fae602f08d041c7cc090a5660a2ce543
nx101 <revelight@gmail.com>
2019-02-05 11:42:06 +0200

    initial

---
+++ src/main/java/com/rvlt/jokesapp/Spring04SpringBootJokeAppApplication.java
@@ -0,0 +1,14 @@
+package com.rvlt.jokesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring04SpringBootJokeAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring04SpringBootJokeAppApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/rvlt/jokesapp/Spring04SpringBootJokeAppApplicationTests.java
@@ -0,0 +1,17 @@
+package com.rvlt.jokesapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Spring04SpringBootJokeAppApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
