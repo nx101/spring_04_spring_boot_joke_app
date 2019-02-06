22c6d0c9643fc0f90782424a4c7d7a0285ffd0a9
nx101 <revelight@gmail.com>
2019-02-06 19:29:53 +0200

    switched to XML config for chuckNorrisQuotes

--- src/main/java/com/rvlt/jokesapp/config/ChuckConfig.java
+++ src/main/java/com/rvlt/jokesapp/config/ChuckConfig.java
@@ -1,15 +1,15 @@
 package com.rvlt.jokesapp.config;
 
 import guru.springframework.norris.chuck.ChuckNorrisQuotes;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration // Java based spring bean configuration
+
+// Switched to legacy XML config, this java config class no longer used or 'seen' by spring
+
+//@Configuration // Java based spring bean configuration
 public class ChuckConfig {
 
-    @Bean // This method provides a ChuckNorrisQuotes bean to the context
-    // Created from an external jar (dependency)
+    //@Bean // This method provides a ChuckNorrisQuotes bean to the context
     public ChuckNorrisQuotes chuckNorrisQuotes(){
-        return new ChuckNorrisQuotes();
+        return new ChuckNorrisQuotes(); // Created from an external jar (dependency)
     }
 }
